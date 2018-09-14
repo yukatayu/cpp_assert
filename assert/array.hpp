dc766b4b548d7c5af27acdc2a204b68ea16a55e9
yukatayu <yukatayu.dev@gmail.com>
2018-09-14 17:36:59 +0900

    added: assertNear: error messages

--- assert/array.hpp
+++ assert/array.hpp
@@ -15,24 +15,33 @@ struct is_subtractable : decltype(is_subtractable_impl::check<T>(std::declval<T>
 
 template <class ElemType, std::enable_if_t<is_subtractable<ElemType>::value, std::nullptr_t> = nullptr>
 bool assertNear(const ElemType& expected, const ElemType& actual, const double tolerance = default_tolerance){
-    if(std::abs(expected - actual) > tolerance)
+    if(std::abs(expected - actual) > tolerance){
+        std::cerr << "\033[91m[Error]\033[0m Assertion failed (tolerance = " << tolerance << ")" << std::endl;
+        std::cerr << "(expected) " << expected << " <> " << actual << " (actual)" << std::endl;
         return false;
+    }
     return true;
 };
 
 template <class ElemType, size_t arraySize>
 bool assertNear(const ElemType (&expected)[arraySize], const ElemType (&actual)[arraySize], const double tolerance = default_tolerance){
-    for(size_t i=0; i<arraySize; ++i)
-        if(!assertNear(expected[i], actual[i], tolerance))
+    for(size_t i=0; i<arraySize; ++i){
+        if(!assertNear(expected[i], actual[i], tolerance)){
+            std::cerr << "\t |> at index " << i << " / " << arraySize << std::endl;
             return false;
+        }
+    }
     return true;
 };
 
 template <class ElemType>
 bool assertNear(size_t arraySize, const ElemType *expected, const ElemType *actual, const double tolerance = default_tolerance){
-    for(size_t i=0; i<arraySize; ++i)
-        if(!assertNear(expected[i], actual[i], tolerance))
+    for(size_t i=0; i<arraySize; ++i){
+        if(!assertNear(expected[i], actual[i], tolerance)){
+            std::cerr << "\t |> at index " << i << " / " << arraySize << std::endl;
             return false;
+        }
+    }
     return true;
 };
 
